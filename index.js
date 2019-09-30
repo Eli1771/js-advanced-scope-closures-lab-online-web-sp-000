17ef152df27485b62c93d8dca11a24189b1ffb63
elijahmm17 <elijahmm17@gmail.com>
2019-09-30 22:10:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function produceDrivingRange(range) {
+  return function(start, end) {
+    const distance = Math.abs(start.slice(0, start.length - 2).toString() - end.slice(0, end.length - 2).toString());
+    const difference = range - distance;
+    return difference >= 0 ? `within range by ${difference}` : `${Math.abs(difference)} blocks out of range`;
+  }
+}
+
+function produceTipCalculator(percent) {
+  return function(fare) {
+    return percent * fare;
+  }
+}
+
+function createDriver() {
+  
+}
